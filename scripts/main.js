c17f860383b26960c59a0408c929272662663f47
AhmedRabi <ahmedrabi100@gmail.com>
2019-06-25 15:54:14 +0200

    CV section

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,31 @@
 $(document).ready(function() {
+  var Oheight = $("section#Portfolio").height();
   var scroll = function() {
-    $(".scrollHolder").animate({ bottom: "0" }, 2000, function() {
-      $(".scrollHolder").animate({ bottom: "30" }, 1500, function() {
+    $(".LoadMore").animate({ bottom: "0" }, 2000, function() {
+      $(".LoadMore").animate({ bottom: "30" }, 1500, function() {
         scroll();
       });
     });
   };
   scroll();
 
-  $(".scrollHolder").click(function() {
-    $('section#Portfolio').animate({
-      height: $('section#Portfolio').get(0).scrollHeight
-  }, 1000, function(){
-      $(this).height('auto');
-  });
-  $('body,html').animate({scrollTop:"200px"},10);
+  $(".LoadMore").click(function() {
+    var section = $("section#Portfolio");
+    var height = section.height();
+    var heightAuto = section.get(0).scrollHeight;
+    if (height != heightAuto - 100) {
+      section.animate(
+        {
+          height: heightAuto
+        },
+        1000
+        // function() {
+        //   $(this).height("auto");
+        // }
+      );
+      $("body,html").animate({ scrollTop: "200px" }, 10);
+    } else {
+      section.animate({ height: Oheight }, 1000);
+    }
   });
 });
